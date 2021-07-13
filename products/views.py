714349b6b54f1d9c537097025ee0edb017b42956
bicks79 <nicholson.lcp@gmail.com>
2021-07-13 22:21:12 +0000

    debug categories error

--- products/views.py
+++ products/views.py
@@ -10,8 +10,14 @@ def all_products(request):
 
     products = Product.objects.all()
     query = None
+    category = None
 
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
