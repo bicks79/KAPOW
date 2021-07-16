136ef4e36f8d587b392f670e4b25d78f8b59d976
bicks79 <nicholson.lcp@gmail.com>
2021-07-16 19:13:02 +0000

    added basket quantity functionality and toasts

--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+from products.models import Product
 
 
 def view_basket(request):
@@ -10,6 +12,7 @@ def view_basket(request):
 def add_to_basket(request, item_id):
     """ A item quantity to shopping basket """
 
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     basket = request.session.get('basket', {})
@@ -18,6 +21,36 @@ def add_to_basket(request, item_id):
         basket[item_id] += quantity
     else:
         basket[item_id] = quantity
+        messages.success(request, f'{ product.name } is now in your collection')
 
     request.session['basket'] = basket
     return redirect(redirect_url)
+
+
+def adjust_basket(request, item_id):
+    """ Adjust the product quantity in the shopping basket """
+
+    quantity = int(request.POST.get('quantity'))
+    basket = request.session.get('basket', {})
+
+    if quantity > 0:
+        basket[item_id] = quantity
+    else:
+        basket.pop(item_id)
+
+    request.session['basket'] = basket
+    return redirect(reverse(view_basket))
+
+
+def remove_from_basket(request, item_id):
+    """ Adjust the product quantity in the shopping basket """
+
+    try:
+        basket = request.session.get('basket', {})
+        basket.pop(item_id)
+
+        request.session['basket'] = basket
+        return HttpResponse(status=200)
+    except Exception as e:
+        print(f"ERROR: {e}")
+        return HttpResponse(status=500)
