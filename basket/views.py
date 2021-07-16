a08d87cef4f60baf60deedd903ef0949c4c0220b
bicks79 <nicholson.lcp@gmail.com>
2021-07-16 20:56:15 +0000

    added extra functionality to add/update/remove functions

--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 from products.models import Product
 
@@ -12,13 +12,14 @@ def view_basket(request):
 def add_to_basket(request, item_id):
     """ A item quantity to shopping basket """
 
-    product = Product.objects.get(pk=item_id)
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     basket = request.session.get('basket', {})
 
     if item_id in list(basket.keys()):
         basket[item_id] += quantity
+        messages.success(request, f'Adjusted { product.name } quantity to {basket[item_id]}')
     else:
         basket[item_id] = quantity
         messages.success(request, f'{ product.name } is now in your collection')
@@ -30,27 +31,32 @@ def add_to_basket(request, item_id):
 def adjust_basket(request, item_id):
     """ Adjust the product quantity in the shopping basket """
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     basket = request.session.get('basket', {})
 
     if quantity > 0:
         basket[item_id] = quantity
+        messages.success(request, f'Adjusted { product.name } quantity to {basket[item_id]}')
     else:
         basket.pop(item_id)
+        messages.success(request, f'{ product.name } is no longer in your collection {basket[item_id]}')
 
     request.session['basket'] = basket
     return redirect(reverse(view_basket))
 
 
 def remove_from_basket(request, item_id):
-    """ Adjust the product quantity in the shopping basket """
+    """ Remove the product from the shopping basket """
 
     try:
+        product = get_object_or_404(Product, pk=item_id)
         basket = request.session.get('basket', {})
         basket.pop(item_id)
+        messages.success(request, f'{ product.name } is no longer in your collection')
 
         request.session['basket'] = basket
         return HttpResponse(status=200)
     except Exception as e:
-        print(f"ERROR: {e}")
+        messages.error(request, f'Your request got lost in the snap!: {e}')
         return HttpResponse(status=500)
